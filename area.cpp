ef21b3fed9fcad36095e0d5d040a5e6840a7ce9c
SashaFlaska <alexander.flaska@ucdenver.edu>
2016-02-07 22:05:31 -0700

    Everything good.

--- area.cpp
+++ area.cpp
@@ -7,15 +7,15 @@
 
 
 
-double findArea(const Point& a, const Point& b, const Point& c)
+double findArea(const Point& a, const Point& b, const Point& c)// finds area
 {
-    double sideA, sideB, sideC;
+    double sideA, sideB, sideC;// sides are computed by using distanceto.
 
     sideA=a.distanceTo(b);
     sideB=b.distanceTo(c);
     sideC=c.distanceTo(a);
 
-    double semiPerimeter;
+    double semiPerimeter;// solves for semi perimeter for use with herons formula to find the area.
     semiPerimeter=(sideA+sideB+sideC)/2;
 
      return sqrt(semiPerimeter*(semiPerimeter-sideA)*(semiPerimeter-sideB)*(semiPerimeter-sideC));
